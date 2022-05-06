1ec8b06d496882a691be8dd88ede225f4bc64443
yzcode <1540032876@qq.com>
2022-05-06 20:54:28 +0800

    feat: order

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -58,14 +58,26 @@
               <div class="order_status_content_status">
                 {{ getStatusLabel(scope.row.status) }}
               </div>
-              <div><el-button type="text">订单详情</el-button></div>
+              <div>
+                <el-button type="text" @click="handleGoOrder(scope.row)"
+                  >订单详情</el-button
+                >
+              </div>
               <div><el-button type="text">查看物流</el-button></div>
               <div><el-button type="text">合同签订</el-button></div>
             </div>
           </template>
         </el-table-column>
         <el-table-column label="交易操作">
-          <el-button type="primary">评价</el-button>
+          <template #default="scope">
+            <el-button
+              v-if="scope.row.status === 1"
+              type="primary"
+              @click="handlePayOrder(scope.row)"
+              >付款</el-button
+            >
+            <el-button v-else type="primary">评价</el-button>
+          </template>
         </el-table-column>
         <el-table-column label="其他">
           <div><el-button type="text">再次购买</el-button></div>
@@ -80,16 +92,55 @@
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import { allStatus, orderStatusList } from "../constants/order";
+import { allStatus, orderStatusList, STORAGE_ORDER } from "../constants/order";
 import MockOrderList from "../mock/mock-order-list.json";
+import { useRouter } from "vue-router";
+import { ElMessage } from "element-plus";
+import lof from "localforage";
+const router = useRouter();
 
 const currentStatus = ref(allStatus.key);
 const allStatusList = computed(() => [allStatus, ...orderStatusList]);
 
-const orderData: any = MockOrderList.list;
+const orderData: any = ref(MockOrderList.list);
+
+getOrderData();
+async function getOrderData() {
+  const data: any = (await lof.getItem(STORAGE_ORDER)) ?? [];
+  orderData.value = [...MockOrderList.list, ...data];
+}
 
 function getStatusLabel(status: number) {
   return orderStatusList.find((item) => item.id === status)?.label ?? "无状态";
+}
+
+async function handlePayOrder(info: any) {
+  const orderData: any = await lof.getItem(STORAGE_ORDER);
+  const newOrderData = orderData.map((item: any) => {
+    if (item.order_id === info.order_id) {
+      return {
+        ...item,
+        status: 6,
+      };
+    }
+    return item;
+  });
+  await lof.setItem(STORAGE_ORDER, newOrderData);
+  ElMessage({
+    message: "支付成功",
+    type: "success",
+    offset: 80,
+  });
+  setTimeout(() => location.reload(), 1000);
+}
+
+function handleGoOrder(info: any) {
+  router.push({
+    name: "order-detail",
+    params: {
+      id: info.order_id,
+    },
+  });
 }
 </script>
 
---
+++ src/views/orderDetail.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>hell0</h1>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+console.log(router)
+</script>
